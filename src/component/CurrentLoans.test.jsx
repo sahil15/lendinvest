dc50dbf108effb7dc3dce2794d508e2a2f6aeeba
sahil15 <sahil.atreja@newgen.co.in>
2021-03-01 13:40:16 +0000

    Add files via upload

--- src/component/CurrentLoans.test.jsx
+++ src/component/CurrentLoans.test.jsx
@@ -3,8 +3,18 @@ import { render, fireEvent, waitFor, screen } from '@testing-library/react'
 import CurrentLoans from './CurrentLoans';
 
 test('loads and displays loans', async () => {
-    const { container, getByText } = render(<CurrentLoans />)
+    const { getByText } = render(<CurrentLoans />)
     expect(getByText('Current Loans')).toBeInTheDocument()
 });
 
 
+test('click on invest button', () => {
+    const { container, getByText } = render(<CurrentLoans />)
+    fireEvent(
+        getByText(container, 'Invest'),
+        new MouseEvent('click', {
+          bubbles: true,
+          cancelable: true,
+        })
+      )
+});
--- src/component/SelectedLoan.test.jsx
+++ src/component/SelectedLoan.test.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, fireEvent, waitFor, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import SelectedLoan from './SelectedLoan';
 
 const props = {
